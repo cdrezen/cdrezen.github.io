aef8e1165feee3fa8d836d024b989a150f41c86b
cdrezen <corentin.drezen@etu.u-bordeaux.fr>
2026-03-24 23:16:31 +0100

    initial inspiration

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import './styles.css';
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='80'>🦆</text></svg>" />
+</svelte:head>
+
+<header>
+	<nav>
+		<span class="navbar_logo">&ltcdrezen/&gt</span>
+		<div class="navbar_list">
+			<a class="navbar_link" href="/">
+				Hello
+			</a>
+			<a class="navbar_link" href="/projects">
+				Projects
+			</a>
+			<a class="navbar_link" href="/contact">
+				Contact
+			</a>
+		</div>
+	</nav>
+</header>
+
+
+<app-root>
+{@render children()}
+</app-root>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,51 @@
+<div class="card">
+    <h2>Card 1</h2>
+    <p>This is the content of the card.</p>
+</div>
+<div class="card">
+    <h2>Card 2</h2>
+    <p>This is the content of the card.</p>
+</div>
+<div class="card">
+    <h2>Card 3</h2>
+    <p>This is the content of the card.</p>
+</div>
+<div class="card">
+    <h2>Card 1</h2>
+    <p>This is the content of the card.</p>
+</div>
+<div class="card">
+    <h2>Card 2</h2>
+    <p>This is the content of the card.</p>
+</div>
+<div class="card">
+    <h2>Card 3</h2>
+    <p>This is the content of the card.</p>
+</div>
+<div class="card">
+    <h2>Card 1</h2>
+    <p>This is the content of the card.</p>
+</div>
+<div class="card">
+    <h2>Card 2</h2>
+    <p>This is the content of the card.</p>
+</div>
+<div class="card">
+    <h2>Card 3</h2>
+    <p>This is the content of the card.</p>
+</div>
+<div class="card">
+    <h2>Card 1</h2>
+    <p>This is the content of the card.</p>
+</div>
+<div class="card">
+    <h2>Card 2</h2>
+    <p>This is the content of the card.</p>
+</div>
+<div class="card">
+    <h2>Card 3</h2>
+    <p>This is the content of the card.</p>
+</div>
+<div class="stars"></div>
+<div class="stars-md"></div>
+<div class="stars-bg"></div>
