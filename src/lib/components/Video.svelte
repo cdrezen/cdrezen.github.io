5f9edd4e7ed123ab779442f081695d3f33858ceb
cdrezen <corentin.drezen@etu.u-bordeaux.fr>
2026-03-28 16:49:36 +0100

    video component + basic flex layout

---
+++ src/lib/components/Video.svelte
@@ -0,0 +1,16 @@
+
+<script lang="ts">
+	let { title, description, videoSrc, poster } = $props();
+</script>
+
+<div>
+	<div class="video-title-desc">
+		<h2>{title}</h2>
+		<p class="video-description">{description}</p>
+	</div>
+	<div class="card video-container">
+		<video controls poster={poster} src={videoSrc}>
+			<track kind="captions" />
+		</video>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,31 @@
+<script lang="ts">
+	import Video from '$lib/components/Video.svelte';
+</script>
+
+<h1>
+<center>Projects</center>
+</h1>
+
+
+<Video
+    title="Xshape"
+    description="Small shapes editor with high level abstraction and design pattern usage"
+    videoSrc="xshape.mp4"
+    poster="cdrezen"
+/>
+
+<Video
+    title="Game dev"
+    description="Gerstner waves with buoyancy and doppler effect simulation"
+    videoSrc="waves.webm"
+    poster="cdrezen"
+/>
+
 <div class="card">
     <h2>Card 1</h2>
     <p>This is the content of the card.</p>
 </div>
+
 <div class="card">
     <h2>Card 2</h2>
     <p>This is the content of the card.</p>
@@ -14,30 +38,14 @@
     <h2>Card 1</h2>
     <p>This is the content of the card.</p>
 </div>
-<div class="card">
-    <h2>Card 2</h2>
-    <p>This is the content of the card.</p>
-</div>
-<div class="card">
-    <h2>Card 3</h2>
-    <p>This is the content of the card.</p>
-</div>
-<div class="card">
-    <h2>Card 1</h2>
-    <p>This is the content of the card.</p>
-</div>
-<div class="card">
-    <h2>Card 2</h2>
-    <p>This is the content of the card.</p>
-</div>
-<div class="card">
-    <h2>Card 3</h2>
-    <p>This is the content of the card.</p>
-</div>
-<div class="card">
-    <h2>Card 1</h2>
-    <p>This is the content of the card.</p>
-</div>
+
+<Video
+    title="Video 1"
+    description="This is a sample video description. The video component includes a title, description, and a video player with standard controls."
+    videoSrc="https://www.w3schools.com/html/mov_bbb.mp4"
+    poster=""
+/>
+
 <div class="card">
     <h2>Card 2</h2>
     <p>This is the content of the card.</p>
