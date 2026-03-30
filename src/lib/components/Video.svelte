3eee40537a1922fa969e2b7655991fab58ae6626
cdrezen <corentin.drezen@etu.u-bordeaux.fr>
2026-03-30 18:22:25 +0200

    video component max size

--- src/lib/components/Video.svelte
+++ src/lib/components/Video.svelte
@@ -3,12 +3,12 @@
 	let { title, description, videoSrc, poster } = $props();
 </script>
 
-<div>
+<div class="video-comp">
 	<div class="video-title-desc">
 		<h2>{title}</h2>
 		<p class="video-description">{description}</p>
 	</div>
-	<div class="card-nobackdrop video-container">
+	<div class="card-nobackdrop video-card">
 		<video controls poster={poster} src={videoSrc}>
 			<track kind="captions" />
 		</video>
